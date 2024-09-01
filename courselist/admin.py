954955bd45672f945815015a96859b9fe78f1ff9
Jiayang Dong <dongjiayang123@hotmail.com>
2024-09-01 09:26:46 -0700

    Created and registered Term and Course models

--- courselist/admin.py
+++ courselist/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Term, Course
 
-# Register your models here.
+admin.site.register(Term)
+admin.site.register(Course)
--- courselist/models.py
+++ courselist/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.urls import reverse
 
+
+class Term(models.Model):
+    SESSION_CHOICES = [('w', 'Winter'), ('s', 'Summer'),]
+    TERM_CHOICES = [(1, 'Term 1'), (1, 'Term 2')]
+
+    year = models.IntegerField()
+    session = models.CharField(max_length=1, choices=SESSION_CHOICES)
+    term = models.PositiveSmallIntegerField(choices=TERM_CHOICES)
+
+    def __str__(self):
+        return f'{self.year} {self.session}T{self.term}'
+
+
 class Course(models.Model):
-    subject = models.CharField(max_length=100)
-    code = models.SmallIntegerField()
+    subject = models.CharField(max_length=10)
+    number = models.IntegerField()
+    full_course_name = models.CharField(max_length=100, null=True, blank=True)
+    terms_offered = models.ManyToManyField(Term, help_text='Select the terms in which the course is offered.')
+
+    def __str__(self):
+        return f'{self.subject} {self.number}'
+    class Meta:
+        unique_together = ('subject', 'number')
