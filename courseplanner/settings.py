370139f48201416adc484cb17c8a788e86c10b72
Jiayang Dong <dongjiayang123@hotmail.com>
2024-09-12 22:06:53 -0700

    migrated to postgreSQL database.

--- courseplanner/settings.py
+++ courseplanner/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'courseplanner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'course_planner',
+        'USER': 'jiayang',
+        'PASSWORD': 'R0ckSt4r',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
